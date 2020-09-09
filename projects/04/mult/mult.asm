c4fd583ce4f2f90d58b8d42f7a51f63c178438c2
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-09-09 09:40:56 +0900

    chapter04 half finish

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+        @R1
+        D=M
+        @n
+        M=D
+        @R2
+        M=0
+(LOOP)
+        @n
+        D=M
+        @END
+        D; JEQ
+        @R0
+        D=M
+        @R2
+        M=M+D
+        @n
+        M=M-1
+        @LOOP
+        0; JMP
+(END)
+        @END
+        0; JMP
