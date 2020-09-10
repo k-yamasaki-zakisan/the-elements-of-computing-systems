206b1db406b46236fd9796e5c45b947fcb84046d
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-09-10 14:04:10 +0900

    chapter04 half finish

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP_KBD)
+        // select color
+        @KBD
+        D=M
+        @SELECT_WHITE
+        D; JEQ
+        @0
+        D=A-1
+        @SET_COLOR
+        0; JMP
+(SELECT_WHITE)
+        @0
+        D=A
+(SET_COLOR)
+        @color
+        M=D
+
+        @SCREEN
+        D=A
+        @pos
+        M=D
+
+        // 32 * 256 = 8192
+        @8192
+        D=A
+        @n
+        M=D
+
+(LOOP_FILL)
+        @n
+        D=M
+        @FILL_END
+        D; JEQ
+
+        // print color
+        @color
+        D=M
+        @pos
+        A=M
+        M=D
+
+        @pos
+        M=M+1
+        @n
+        M=M-1
+
+        @LOOP_FILL
+        0; JMP
+(FILL_END)
+        @LOOP_KBD
+        0; JMP
